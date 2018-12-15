a645809efc738bc832ac958dc2a3073dbce98d3a
zentim <zentim3248@gmail.com>
2018-12-15 18:52:52 +0800

    list all record

--- orderhistory/index.php
+++ orderhistory/index.php
@@ -1,3 +1,22 @@
+<?php
+  require("./include/variable_setting.php");
+?>
+
+<?php
+  /**
+   * connect to db
+   */
+  require("./include/db_connection.php");
+?>
+
+<?php
+  // 取得SQL指令
+  $SQL = "SELECT * FROM $TABLE_NAME";
+  $SQL = stripslashes($SQL);
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -18,5 +37,61 @@
     </form>
     <hr>
 
+<?php
+    echo "<div>";
+
+    // 執行SQL查詢
+    $result = mysqli_query($link, $SQL);
+    // 一筆一筆的以表格顯示記錄
+    echo "<table border=1><tr>";
+    // 顯示欄位名稱
+    while ( $meta = mysqli_fetch_field($result) ) {
+      echo "<td>".$meta->name."</td>";
+    }
+    echo "<td>操作</td>";
+    echo "<td>操作</td>";
+    echo "</tr>"; // 取得欄位數
+    $total_fields = mysqli_num_fields($result);
+    // 顯示每一筆記錄
+    while ($row = mysqli_fetch_row($result)) {
+      echo "<tr>"; // 顯示每一筆記錄的欄位值
+      for ( $i = 0; $i <= $total_fields-1; $i++ ) {
+        echo "<td>" . $row[$i] . "</td>";
+      }
+      // delete button
+      echo "<td>";
+      echo '<form action="controller.php" method="post">';
+      echo '<input type="hidden" name="'. $ID . '" value="'. $row[0] .'">';
+      echo '<input type="submit" name="operation_delete" value="刪除"><br>';
+      echo '</form>';
+      echo "</td>";
+
+      // update button
+      echo "<td>";
+      echo '<form action="controller.php" method="post">';
+      echo '<input type="hidden" name="'. $ID . '" value="'. $row[0] .'">';
+      echo '<input type="submit" name="operation_update" value="修改"><br>';
+      echo '</form>';
+      echo "</td>";
+
+      echo "</tr>";
+    }
+    echo "</table>";
+    echo "<hr>";
+    echo "</div>";
+    mysqli_free_result($result); // 釋放佔用記憶體
+?>
+
+
 </body>
 </html>
+
+
+
+
+<?php
+  /**
+   * close db
+   */
+  require("./include/db_close.php");
+?>

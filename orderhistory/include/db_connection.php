4c79ce859f16781a10eb5f87c90e5716700d5f48
zentim <zentim3248@gmail.com>
2018-12-21 20:37:13 +0800

    remove comment

--- orderhistory/include/db_connection.php
+++ orderhistory/include/db_connection.php
@@ -3,7 +3,7 @@
 // 開啟MySQL的資料庫連接
 $link = @mysqli_connect("localhost", "root", "")
     or die("無法開啟MySQL資料庫連接!<br/>");
-mysqli_select_db($link, "dbms_hw3");  // 選擇myschool資料庫
+mysqli_select_db($link, "dbms_hw3");
 
 //送出UTF8編碼的MySQL指令
 mysqli_query($link, 'SET NAMES utf8');
